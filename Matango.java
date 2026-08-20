cdee139ea587f8c20544ad68a70c90282568cfb3
[taikikanaya] <[kanayataiki0330@gmail.com]>
2026-08-20 11:33:36 +0900

    Create Matango.java

---
+++ Matango.java
@@ -0,0 +1,12 @@
+package chap08_02;
+
+public class Matango {
+	int hp;
+	//int level = 10;
+	final int LEVEL = 10 ;
+	char suffix;
+	
+	public void run() {
+		System.out.println("おばけキノコ" + this.suffix + "は、逃げ出した！");
+	}
+}
