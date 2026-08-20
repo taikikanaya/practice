3120cb07d0e32acd9d2c5996c23c71ff4c9b2e00
taikikanaya <kanayataiki0330@gmail.com>
2026-08-20 10:25:44 +0900

    Create TngibleAsset class with properties and methods
    Fix typos in TngibleAsset class and its methods.

---
+++ TngibleAsset.java
@@ -0,0 +1,23 @@
+public abstract class TngibleAsset{
+  private String name;
+  private int price;
+  private String color;
+
+  public TngibleAsset(String name,int price,String color){
+    this.name = name;
+    this.price = price;
+    thhis.color = color;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int price(){
+    return this.price;
+  }
+
+  public String color(){
+    retrun this.color;
+  }
+}
